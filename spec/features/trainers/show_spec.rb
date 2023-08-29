1f3b7193771d8555d1e3ecf7aca4ff4ed3a29c95
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-08-29 15:28:04 -0600

    test: Add another trainer to test in show_spec.rb test

--- spec/features/trainers/show_spec.rb
+++ spec/features/trainers/show_spec.rb
@@ -8,13 +8,22 @@ RSpec.describe 'trainers show page' do
   it 'shows trainers id including the trainers attributes' do 
     visit "/trainers/#{@ash.id}"
     
-    save_and_open_page
-
     expect(page).to have_content(@ash.name)
     expect(page).to have_content(@ash.age)
     expect(page).to have_content(@ash.hometown)
     expect(page).to have_content(@ash.gym_badges_collected)
     expect(page).to have_content(@ash.pokedex_count)
     expect(page).to have_content(@ash.has_bike)
+      
+    visit "/trainers/#{@gary.id}"
+    
+    expect(page).to have_content(@gary.name)
+    expect(page).to have_content(@gary.age)
+    expect(page).to have_content(@gary.hometown)
+    expect(page).to have_content(@gary.gym_badges_collected)
+    expect(page).to have_content(@gary.pokedex_count)
+    expect(page).to have_content(@gary.has_bike)
+    
+    # save_and_open_page
   end
 end
